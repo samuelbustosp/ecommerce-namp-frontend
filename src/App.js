31b2733d94b3b9cbe7b8a9c8f40234af6c35b472
samuelbustosp <samuelbustospuntis@gmail.com>
2024-09-23 20:15:22 -0300

    New Package Private Pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/private/Dashboard';
 import AdminLayout from './layout/admin/AdminLayout';
-import ProductPage from './pages/ProductPage';
-import CategoryPage from './pages/CategoryPage';
-import SubcategoryPage from './pages/SubcategoryPage';
+import ProductPage from './pages/private/ProductPage';
+import CategoryPage from './pages/private/CategoryPage';
+import SubcategoryPage from './pages/private/SubcategoryPage';
 import CategoryForm from './components/admin/category/AddCategoryModal';
 import ClientLayout from './layout/client/ClientLayout';
-import Home from './pages/client/Home';
-import CategoryDetailPage from './pages/client/CategoryDetailPage';
-import CategoryMenuContainer from './components/client/category/CategoryMenuContainer'
-import CategoryListPage from './pages/client/CategoryListPage';
+import Home from './pages/public/Home';
+import CategoryDetailPage from './pages/public/CategoryDetailPage';
+import CategoryListPage from './pages/public/CategoryListPage';
 
 function App() {
-  
   return (
     <div className="App">
       <BrowserRouter>
@@ -31,7 +29,6 @@ function App() {
             <Route path="categoria" element={<CategoryListPage/>}/>
             <Route path="categoria/:name" element={<CategoryDetailPage/>}/>
           </Route>
-
         </Routes>
       </BrowserRouter>
     </div>
